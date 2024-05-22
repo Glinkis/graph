72da60b45784ae4e2cccede5de2e7235409cf405
Victor Glindås <glinkis@gmail.com>
2024-05-22 21:46:54 +0200

    chor: build once

--- canvas/build.ts
+++ canvas/build.ts
@@ -1,13 +1,21 @@
 import { watch } from "node:fs";
 import html from "bun-plugin-html";
 
-console.log("Building canvas...");
-
-watch(import.meta.dir, async () => {
-  console.log("Rebuilding canvas...");
+async function build() {
   await Bun.build({
     entrypoints: ["./canvas/index.html"],
     outdir: "./dist",
     plugins: [html()],
   });
-});
+}
+
+if ("BUILD_ONCE" in process.env) {
+  console.log("Building canvas once...");
+  await build();
+} else {
+  console.log("Building canvas in watch mode...");
+  watch(import.meta.dir, async () => {
+    console.log("Rebuilding canvas...");
+    await build();
+  });
+}
