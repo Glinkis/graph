ed4e6ab32d1004942834be55b971289296ba71c8
Victor Glindås <glinkis@gmail.com>
2024-05-22 21:35:07 +0200

    feat: add interactivity

--- canvas/index.ts
+++ canvas/index.ts
@@ -8,10 +8,19 @@ const COLOR_PALETTE = {
   QUATERNARY: "#f7f9fb",
 };
 
+const NODE_INNER_RADIUS = 16;
+const NODE_BORDER_WIDTH = 8;
+const NODE_OUTER_RADIUS = NODE_INNER_RADIUS + NODE_BORDER_WIDTH;
+
+const EDGE_WIDTH = 8;
+
 const canvas = document.createElement("canvas");
 document.body.appendChild(canvas);
 
 const ctx = canvas.getContext("2d");
+if (ctx == null) {
+  throw new Error("2d context not supported");
+}
 
 const graph = new Graph();
 
@@ -33,7 +42,102 @@ const canvasPosition: Position = {
   y: 0,
 };
 
+const pointer: {
+  moveEvent: PointerEvent | null;
+} = {
+  moveEvent: null,
+};
+
+const nodePositions = new Map<string, Position>([
+  [node1.id, { x: 100, y: 100 }],
+  [node2.id, { x: 200, y: 200 }],
+  [node3.id, { x: 300, y: 100 }],
+  [node4.id, { x: 400, y: 200 }],
+]);
+
+const dragPositions: {
+  source: Position | null;
+  target: Position | null;
+} = {
+  source: null,
+  target: null,
+};
+
+function getDistanceFromPointer(event: PointerEvent, position: Position) {
+  return Math.hypot(
+    event.clientX - position.x - canvasPosition.x,
+    event.clientY - position.y - canvasPosition.y,
+  );
+}
+
+canvas.addEventListener("pointermove", (event: PointerEvent) => {
+  pointer.moveEvent = event;
+});
+
 canvas.addEventListener("pointerdown", (event: PointerEvent) => {
+  if (event.button === 0) {
+    for (const node of nodePositions.values()) {
+      const distance = getDistanceFromPointer(event, node);
+
+      if (distance < NODE_OUTER_RADIUS) {
+        const initialPosition: Position = {
+          x: event.clientX - node.x,
+          y: event.clientY - node.y,
+        };
+
+        const move = (event: PointerEvent) => {
+          node.x = event.clientX - initialPosition.x;
+          node.y = event.clientY - initialPosition.y;
+        };
+
+        const cleanup = () => {
+          removeEventListener("pointermove", move);
+          removeEventListener("pointerup", cleanup);
+        };
+
+        addEventListener("pointermove", move);
+        addEventListener("pointerup", cleanup);
+        return;
+      }
+    }
+  }
+
+  if (event.button === 1) {
+    for (const [node1Id, node1] of nodePositions) {
+      const distance = getDistanceFromPointer(event, node1);
+
+      if (distance < NODE_OUTER_RADIUS) {
+        const move = (event: PointerEvent) => {
+          dragPositions.source = node1;
+          dragPositions.target = {
+            x: event.clientX - canvasPosition.x,
+            y: event.clientY - canvasPosition.y,
+          };
+        };
+
+        const cleanup = (event: PointerEvent) => {
+          for (const [node2Id, node2] of nodePositions) {
+            const distance = getDistanceFromPointer(event, node2);
+
+            if (distance < NODE_OUTER_RADIUS) {
+              graph.addEdge(node1Id, node2Id);
+            }
+          }
+
+          removeEventListener("pointermove", move);
+          removeEventListener("pointerup", cleanup);
+
+          dragPositions.source = null;
+          dragPositions.target = null;
+        };
+
+        addEventListener("pointermove", move);
+        addEventListener("pointerup", cleanup);
+        return;
+      }
+    }
+  }
+
   const initialPosition: Position = {
     x: event.clientX - canvasPosition.x,
     y: event.clientY - canvasPosition.y,
@@ -53,18 +157,39 @@ canvas.addEventListener("pointerdown", (event: PointerEvent) => {
   addEventListener("pointerup", cleanup);
 });
 
-const nodePositions = new Map<string, Position>([
-  [node1.id, { x: 100, y: 100 }],
-  [node2.id, { x: 200, y: 200 }],
-  [node3.id, { x: 300, y: 100 }],
-  [node4.id, { x: 400, y: 200 }],
-]);
+const drawNode = (position: Position) => {
+  const x = position.x + canvasPosition.x;
+  const y = position.y + canvasPosition.y;
 
-function render() {
-  if (ctx == null) {
-    throw new Error("2d context not supported");
+  ctx.beginPath();
+  ctx.arc(x, y, NODE_INNER_RADIUS, 0, Math.PI * 2);
+
+  if (pointer.moveEvent) {
+    const distance = getDistanceFromPointer(pointer.moveEvent, position);
+
+    if (distance < NODE_OUTER_RADIUS) {
+      ctx.lineWidth = NODE_BORDER_WIDTH;
+      ctx.strokeStyle = COLOR_PALETTE.QUATERNARY;
+      ctx.stroke();
+    }
   }
 
+  ctx.fillStyle = COLOR_PALETTE.PRIMARY;
+  ctx.fill();
+};
+
+const drawEdge = (source: Position, target: Position) => {
+  ctx.strokeStyle = COLOR_PALETTE.PRIMARY;
+  ctx.lineWidth = EDGE_WIDTH;
+  ctx.lineCap = "round";
+
+  ctx.beginPath();
+  ctx.moveTo(source.x + canvasPosition.x, source.y + canvasPosition.y);
+  ctx.lineTo(target.x + canvasPosition.x, target.y + canvasPosition.y);
+  ctx.stroke();
+};
+
+const render = () => {
   canvas.width = devicePixelRatio * innerWidth;
   canvas.height = devicePixelRatio * innerHeight;
 
@@ -74,55 +199,34 @@ function render() {
   ctx.fillStyle = COLOR_PALETTE.BACKGROUND;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  ctx.scale(devicePixelRatio, devicePixelRatio);
-
   ctx.translate(
     devicePixelRatio / canvasPosition.x,
     devicePixelRatio / canvasPosition.y,
   );
-
-  for (const node of graph.nodes) {
-    const position = nodePositions.get(node.id);
-
-    if (position) {
-      ctx.lineWidth = 4;
-      ctx.strokeStyle = COLOR_PALETTE.QUATERNARY;
-      ctx.fillStyle = COLOR_PALETTE.PRIMARY;
-      ctx.beginPath();
-      ctx.arc(
-        position.x + canvasPosition.x,
-        position.y + canvasPosition.y,
-        10,
-        0,
-        Math.PI * 2,
-      );
-      ctx.fill();
-      ctx.stroke();
-    }
-  }
+  ctx.scale(devicePixelRatio, devicePixelRatio);
 
   for (const edge of graph.edges) {
     const sourcePosition = nodePositions.get(edge.source);
     const targetPosition = nodePositions.get(edge.target);
 
     if (sourcePosition && targetPosition) {
-      ctx.strokeStyle = COLOR_PALETTE.PRIMARY;
-      ctx.lineWidth = 4;
-      ctx.lineCap = "round";
-      ctx.beginPath();
-      ctx.moveTo(
-        sourcePosition.x + canvasPosition.x,
-        sourcePosition.y + canvasPosition.y,
-      );
-      ctx.lineTo(
-        targetPosition.x + canvasPosition.x,
-        targetPosition.y + canvasPosition.y,
-      );
-      ctx.stroke();
+      drawEdge(sourcePosition, targetPosition);
     }
   }
 
+  for (const node of graph.nodes) {
+    const position = nodePositions.get(node.id);
+
+    if (position) {
+      drawNode(position);
+    }
+  }
+
+  if (dragPositions.source && dragPositions.target) {
+    drawEdge(dragPositions.source, dragPositions.target);
+  }
+
   requestAnimationFrame(render);
-}
+};
 
 requestAnimationFrame(render);
